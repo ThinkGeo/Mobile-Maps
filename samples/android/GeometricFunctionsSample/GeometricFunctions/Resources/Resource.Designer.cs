a7c1d33b9329d7f87a749545d8127bd20988ab23
Ryan Duan <ryanduan@thinkgeo.com>
2020-04-09 16:40:37 -0500

    thinkgeo/public/above-the-fold#85
    Add mapsuitelicense file to assets

--- samples/android/GeometricFunctionsSample/GeometricFunctions/Resources/Resource.Designer.cs
+++ samples/android/GeometricFunctionsSample/GeometricFunctions/Resources/Resource.Designer.cs
@@ -15,7 +15,7 @@ namespace GeometricFunctions
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
@@ -64,104 +64,104 @@ namespace GeometricFunctions
 		public partial class Drawable
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int bbox = 2130837504;
+			// aapt resource value: 0x7F010000
+			public const int bbox = 2130771968;
 			
-			// aapt resource value: 0x7f020001
-			public const int buffer = 2130837505;
+			// aapt resource value: 0x7F010001
+			public const int buffer = 2130771969;
 			
-			// aapt resource value: 0x7f020002
-			public const int centerPoint = 2130837506;
+			// aapt resource value: 0x7F010002
+			public const int centerPoint = 2130771970;
 			
-			// aapt resource value: 0x7f020003
-			public const int clip = 2130837507;
+			// aapt resource value: 0x7F010003
+			public const int clip = 2130771971;
 			
-			// aapt resource value: 0x7f020004
-			public const int Compass = 2130837508;
+			// aapt resource value: 0x7F010004
+			public const int Compass = 2130771972;
 			
-			// aapt resource value: 0x7f020005
-			public const int convexHull = 2130837509;
+			// aapt resource value: 0x7F010005
+			public const int convexHull = 2130771973;
 			
-			// aapt resource value: 0x7f020006
-			public const int detail40 = 2130837510;
+			// aapt resource value: 0x7F010006
+			public const int detail40 = 2130771974;
 			
-			// aapt resource value: 0x7f020007
-			public const int difference = 2130837511;
+			// aapt resource value: 0x7F010007
+			public const int difference = 2130771975;
 			
-			// aapt resource value: 0x7f020008
-			public const int getArea = 2130837512;
+			// aapt resource value: 0x7F010008
+			public const int getArea = 2130771976;
 			
-			// aapt resource value: 0x7f020009
-			public const int getLength = 2130837513;
+			// aapt resource value: 0x7F010009
+			public const int getLength = 2130771977;
 			
-			// aapt resource value: 0x7f02000a
-			public const int Icon = 2130837514;
+			// aapt resource value: 0x7F01000A
+			public const int Icon = 2130771978;
 			
-			// aapt resource value: 0x7f02000b
-			public const int icon_mapsuite_default256x256 = 2130837515;
+			// aapt resource value: 0x7F010014
+			public const int iconstyle = 2130771988;
 			
-			// aapt resource value: 0x7f02000c
-			public const int icon_mapsuite_default512x512 = 2130837516;
+			// aapt resource value: 0x7F01000B
+			public const int icon_mapsuite_default256x256 = 2130771979;
 			
-			// aapt resource value: 0x7f02000d
-			public const int icon_mapsuite_globe = 2130837517;
+			// aapt resource value: 0x7F01000C
+			public const int icon_mapsuite_default512x512 = 2130771980;
 			
-			// aapt resource value: 0x7f02000e
-			public const int icon_mapsuite_globe_Pressed = 2130837518;
+			// aapt resource value: 0x7F01000D
+			public const int icon_mapsuite_globe = 2130771981;
 			
-			// aapt resource value: 0x7f02000f
-			public const int icon_mapsuite_Minus = 2130837519;
+			// aapt resource value: 0x7F01000E
+			public const int icon_mapsuite_globe_Pressed = 2130771982;
 			
-			// aapt resource value: 0x7f020010
-			public const int icon_mapsuite_Minus_Pressed = 2130837520;
+			// aapt resource value: 0x7F01000F
+			public const int icon_mapsuite_Minus = 2130771983;
 			
-			// aapt resource value: 0x7f020011
-			public const int icon_mapsuite_noImageTile = 2130837521;
+			// aapt resource value: 0x7F010010
+			public const int icon_mapsuite_Minus_Pressed = 2130771984;
 			
-			// aapt resource value: 0x7f020012
-			public const int icon_mapsuite_Plus = 2130837522;
+			// aapt resource value: 0x7F010011
+			public const int icon_mapsuite_noImageTile = 2130771985;
 			
-			// aapt resource value: 0x7f020013
-			public const int icon_mapsuite_Plus_Pressed = 2130837523;
+			// aapt resource value: 0x7F010012
+			public const int icon_mapsuite_Plus = 2130771986;
 			
-			// aapt resource value: 0x7f020014
-			public const int iconstyle = 2130837524;
+			// aapt resource value: 0x7F010013
+			public const int icon_mapsuite_Plus_Pressed = 2130771987;
 			
-			// aapt resource value: 0x7f020015
-			public const int lineOnLine = 2130837525;
+			// aapt resource value: 0x7F010015
+			public const int lineOnLine = 2130771989;
 			
-			// aapt resource value: 0x7f020016
-			public const int MapSuite = 2130837526;
+			// aapt resource value: 0x7F010016
+			public const int MapSuite = 2130771990;
 			
-			// aapt resource value: 0x7f020017
-			public const int More = 2130837527;
+			// aapt resource value: 0x7F010017
+			public const int More = 2130771991;
 			
-			// aapt resource value: 0x7f020018
-			public const int more40 = 2130837528;
+			// aapt resource value: 0x7F010018
+			public const int more40 = 2130771992;
 			
-			// aapt resource value: 0x7f020019
-			public const int rotate = 2130837529;
+			// aapt resource value: 0x7F010019
+			public const int rotate = 2130771993;
 			
-			// aapt resource value: 0x7f02001a
-			public const int scale = 2130837530;
+			// aapt resource value: 0x7F01001A
+			public const int scale = 2130771994;
 			
-			// aapt resource value: 0x7f02001b
-			public const int settings40 = 2130837531;
+			// aapt resource value: 0x7F01001B
+			public const int settings40 = 2130771995;
 			
-			// aapt resource value: 0x7f02001c
-			public const int shortestLine = 2130837532;
+			// aapt resource value: 0x7F01001C
+			public const int shortestLine = 2130771996;
 			
-			// aapt resource value: 0x7f02001d
-			public const int simplify = 2130837533;
+			// aapt resource value: 0x7F01001D
+			public const int simplify = 2130771997;
 			
-			// aapt resource value: 0x7f02001e
-			public const int SliderLineBackground = 2130837534;
+			// aapt resource value: 0x7F01001E
+			public const int SliderLineBackground = 2130771998;
 			
-			// aapt resource value: 0x7f02001f
-			public const int snapGps = 2130837535;
+			// aapt resource value: 0x7F01001F
+			public const int snapGps = 2130771999;
 			
-			// aapt resource value: 0x7f020020
-			public const int union = 2130837536;
+			// aapt resource value: 0x7F010020
+			public const int union = 2130772000;
 			
 			static Drawable()
 			{
@@ -176,41 +176,41 @@ namespace GeometricFunctions
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050008
-			public const int ExecuteButton = 2131034120;
+			// aapt resource value: 0x7F020000
+			public const int ExecuteButton = 2130837504;
 			
-			// aapt resource value: 0x7f050005
-			public const int LabelStylingLayout = 2131034117;
+			// aapt resource value: 0x7F020001
+			public const int LabelStylingLayout = 2130837505;
 			
-			// aapt resource value: 0x7f050009
-			public const int MapContainerView = 2131034121;
+			// aapt resource value: 0x7F020005
+			public const int mainLinearLayout = 2130837509;
 			
-			// aapt resource value: 0x7f050006
-			public const int SampleListButton = 2131034118;
+			// aapt resource value: 0x7F020002
+			public const int MapContainerView = 2130837506;
 			
-			// aapt resource value: 0x7f050007
-			public const int TitleTextView = 2131034119;
+			// aapt resource value: 0x7F020003
+			public const int SampleListButton = 2130837507;
 			
-			// aapt resource value: 0x7f050004
-			public const int mainLinearLayout = 2131034116;
+			// aapt resource value: 0x7F020006
+			public const int sampleListMoreButton = 2130837510;
 			
-			// aapt resource value: 0x7f050003
-			public const int sampleListMoreButton = 2131034115;
+			// aapt resource value: 0x7F020007
+			public const int sampleListTitle = 2130837511;
 			
-			// aapt resource value: 0x7f050001
-			public const int sampleListTitle = 2131034113;
+			// aapt resource value: 0x7F020008
+			public const int sampleListView = 2130837512;
 			
-			// aapt resource value: 0x7f050002
-			public const int sampleListView = 2131034114;
+			// aapt resource value: 0x7F020009
+			public const int slider_view = 2130837513;
 			
-			// aapt resource value: 0x7f050000
-			public const int slider_view = 2131034112;
+			// aapt resource value: 0x7F020004
+			public const int TitleTextView = 2130837508;
 			
-			// aapt resource value: 0x7f05000b
-			public const int uploadDataTextView = 2131034123;
+			// aapt resource value: 0x7F02000A
+			public const int uploadDataTextView = 2130837514;
 			
-			// aapt resource value: 0x7f05000a
-			public const int uploadProgressBar = 2131034122;
+			// aapt resource value: 0x7F02000B
+			public const int uploadProgressBar = 2130837515;
 			
 			static Id()
 			{
@@ -225,16 +225,16 @@ namespace GeometricFunctions
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
+			// aapt resource value: 0x7F030000
 			public const int ButtonBackgroundSelector = 2130903040;
 			
-			// aapt resource value: 0x7f030001
+			// aapt resource value: 0x7F030001
 			public const int Main = 2130903041;
 			
-			// aapt resource value: 0x7f030002
+			// aapt resource value: 0x7F030002
 			public const int SampleBaseLayout = 2130903042;
 			
-			// aapt resource value: 0x7f030003
+			// aapt resource value: 0x7F030003
 			public const int SplashLayout = 2130903043;
 			
 			static Layout()
@@ -250,11 +250,11 @@ namespace GeometricFunctions
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7F040000
+			public const int ApplicationName = 2130968576;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7F040001
+			public const int Hello = 2130968577;
 			
 			static String()
 			{
