a7b081882e069ecd692db22e8be0851c4631f9eb
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-07-28 16:57:27 -0500

    thinkgeo/public/above-the-fold#377 Added the projection samples.

---
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/ProjectingData/SetLayerProjectionSample.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkGeo.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HowDoISample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SetLayerProjectionSample : ContentPage
+    {
+        public SetLayerProjectionSample()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ...
+        /// </summary>
+        private void MapView_Loaded(object sender, EventArgs e)
+        {
+            //// Create the background world maps using vector tiles requested from the ThinkGeo Cloud Service. 
+            //ThinkGeoCloudVectorMapsOverlay thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
+            //mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+
+            //// Set the Map Unit to meters (Spherical Mercator)
+            //mapView.MapUnit = GeographyUnit.Meter;
+
+            //// Create an overlay that we can add feature layers to, and add it to the MapView
+            //LayerOverlay subdivisionsOverlay = new LayerOverlay();
+            //mapView.Overlays.Add("Frisco Subdivisions Overlay", subdivisionsOverlay);
+
+            //// Reproject a shapefile and set the extent
+            //ReprojectFeaturesFromShapefile();
+        }
+
+        private void ReprojectFeaturesFromShapefile()
+        {
+            //// Create a feature layer to hold the Frisco subdivisions data
+            //ShapeFileFeatureLayer subdivisionsLayer = new ShapeFileFeatureLayer(@"../../../Data/Shapefile/Subdivisions.shp");
+
+            //// Create a new ProjectionConverter to convert between Texas North Central (2276) and Spherical Mercator (3857)
+            //ProjectionConverter projectionConverter = new ProjectionConverter(2276, 3857);
+            //subdivisionsLayer.FeatureSource.ProjectionConverter = projectionConverter;
+
+            //// Add a style to use to draw the Frisco subdivions polygons
+            //subdivisionsLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyle.CreateSimpleAreaStyle(GeoColor.FromArgb(50, GeoColors.MediumPurple), GeoColors.MediumPurple, 2);
+
+            //// Apply the styles across all zoom levels
+            //subdivisionsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            //// Get the overlay we prepared from the MapView, and add the subdivisions ShapeFileFeatureLayer to it
+            //LayerOverlay subdivisionsOverlay = (LayerOverlay)mapView.Overlays["Frisco Subdivisions Overlay"];
+            //subdivisionsOverlay.Layers.Clear();
+            //subdivisionsOverlay.Layers.Add("Frisco Subdivisions", subdivisionsLayer);
+
+            //// Set the map to the extent of the subdivisions features and refresh the map
+            //subdivisionsLayer.Open();
+            //mapView.CurrentExtent = subdivisionsLayer.GetBoundingBox();
+            //subdivisionsLayer.Close();
+            //mapView.Refresh();
+        }
+    }
+}
