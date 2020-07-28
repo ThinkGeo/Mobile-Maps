d93b2033405be4230bf5b62693f3f4c86d6ef289
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-07-28 13:59:00 -0500

    thinkgeo/public/above-the-fold#377 Added the Raster layer samples.

---
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/DisplayingLayers/Raster/CommonRasterLayersSample.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkGeo.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HowDoISample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommonRasterLayersSample : ContentPage
+    {
+        public CommonRasterLayersSample()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ...
+        /// </summary>
+        private void MapView_Loaded(object sender, EventArgs e)
+        {
+            //// It is important to set the map unit first to either feet, meters or decimal degrees.
+            //mapView.MapUnit = GeographyUnit.Meter;
+
+            //// Create the background world maps using vector tiles requested from the ThinkGeo Cloud Service and add it to the map.
+            //ThinkGeoCloudVectorMapsOverlay thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
+            //mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+
+            //// Create a new overlay that will hold our new layer and add it to the map.
+            //LayerOverlay layerOverlay = new LayerOverlay();
+            //mapView.Overlays.Add(layerOverlay);
+
+            //// Create the new layer and dd the layer to the overlay we created earlier.
+            //NativeImageRasterLayer commonRasterLayer = new NativeImageRasterLayer("../../../Data/Jpg/m_3309650_sw_14_1_20160911_20161121.jpg");
+            //layerOverlay.Layers.Add(commonRasterLayer);
+
+            //// Set the map view current extent to a slightly zoomed in area of the image.
+            //mapView.CurrentExtent = new RectangleShape(-10783910.2966461, 3917274.29233111, -10777309.4670677, 3912119.9131963);
+
+            //// Refresh the map.
+            //mapView.Refresh();
+        }
+    }
+}
---
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/DisplayingLayers/Raster/ECWLayerSample.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkGeo.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HowDoISample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ECWLayerSample : ContentPage
+    {
+        public ECWLayerSample()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ...
+        /// </summary>
+        private void MapView_Loaded(object sender, EventArgs e)
+        {
+            //// It is important to set the map unit first to either feet, meters or decimal degrees.
+            //mapView.MapUnit = GeographyUnit.DecimalDegree;
+
+            //// Create a new overlay that will hold our new layer and add it to the map.
+            //LayerOverlay layerOverlay = new LayerOverlay();
+            //mapView.Overlays.Add(layerOverlay);
+
+            //// Create the new layer and dd the layer to the overlay we created earlier.
+            //EcwRasterLayer ecwRasterLayer = new EcwRasterLayer("../../../Data/Ecw/World.ecw");
+            //layerOverlay.Layers.Add(ecwRasterLayer);
+
+            //// Set the map view current extent to a slightly zoomed in area of the image.
+            //mapView.CurrentExtent = new RectangleShape(-90.5399054799761, 68.8866552710533, 57.5181302343096, -43.7137911575181);
+
+            //// Refresh the map.
+            //mapView.Refresh();
+        }
+    }
+}
