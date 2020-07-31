087e8c22d9830c1b600429b98f8414c667dd2ddf
JoshuaRehagen <joshrehagen@thinkGeo.com>
2020-07-31 10:31:57 -0500

    thinkgeo/public/above-the-fold#377 Added UsingQueryTools samples.

---
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/UsingQueryTools/GetAllFeatureDataSample.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkGeo.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HowDoISample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SampleTemplate : ContentPage
+    {
+        public SampleTemplate()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ...
+        /// </summary>
+        private void MapView_Loaded(object sender, EventArgs e)
+        {
+            //// Set the map's unit of measurement to meters(Spherical Mercator)
+            //mapView.MapUnit = GeographyUnit.Meter;
+
+            //// Add Cloud Maps as a background overlay
+            //var thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("itZGOI8oafZwmtxP-XGiMvfWJPPc-dX35DmESmLlQIU~", "bcaCzPpmOG6le2pUz5EAaEKYI-KSMny_WxEAe7gMNQgGeN9sqL12OA~~", ThinkGeoCloudVectorMapsMapType.Light);
+            //mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+
+            //// Set the map extent
+            //mapView.CurrentExtent = new RectangleShape(-10786436, 3918518, -10769429, 3906002);
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
