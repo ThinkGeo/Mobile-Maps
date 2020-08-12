8973f5a99b046263909740d3d7a91ce7f5e5b93e
Kyle <kyleday@thinkgeo.com>
2020-08-12 10:02:20 -0500

    thinkgeo/public/above-the-fold#377 removed detail page caching mechanism until we can find out why we are getting map disposed exceptions

--- samples/xamarin-forms/HowDoISample/HowDoISample/Views/MainPage.xaml.cs
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Views/MainPage.xaml.cs
@@ -14,8 +14,6 @@ namespace ThinkGeo.UI.Xamarin.HowDoI
     [DesignTimeVisible(false)]
     public partial class MainPage : MasterDetailPage
     {
-        Dictionary<string, NavigationPage> MenuPages = new Dictionary<string, NavigationPage>();
-
         public MainPage()
         {
             InitializeComponent();
@@ -25,18 +23,12 @@ namespace ThinkGeo.UI.Xamarin.HowDoI
 
         public async Task NavigateFromMenu(string id)
         {
-            if (!MenuPages.ContainsKey(id))
-            {
-                var type = Type.GetType(id);
-                var samplePage = (Page)Activator.CreateInstance(type);
-                MenuPages.Add(id, new NavigationPage(samplePage));
-            }
+            var type = Type.GetType(id);
+            var samplePage = new NavigationPage((Page)Activator.CreateInstance(type));
 
-            var newPage = MenuPages[id];
-
-            if (newPage != null && Detail != newPage)
+            if (samplePage != null && Detail != samplePage)
             {
-                Detail = newPage;
+                Detail = samplePage;
 
                 if (Device.RuntimePlatform == Device.Android)
                     await Task.Delay(100);
