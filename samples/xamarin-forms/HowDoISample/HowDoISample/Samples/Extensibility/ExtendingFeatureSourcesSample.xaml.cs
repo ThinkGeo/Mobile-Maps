7a2cbe827343bba2c33bbb5e270eb356f6f41755
JoshuaRehagen <joshrehagen@thinkGeo.com>
2021-01-15 17:22:48 -0600

    thinkgeo/public/above-the-fold#415 Added the extensibility and Creating styles sample.

--- samples/xamarin-forms/HowDoISample/HowDoISample/Samples/Extensibility/ExtendingFeatureSourcesSample.xaml.cs
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/Extensibility/ExtendingFeatureSourcesSample.xaml.cs
@@ -32,10 +32,10 @@ namespace ThinkGeo.UI.XamarinForms.HowDoI
             mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
 
             // See the implementation of the new layer and feature source below.
-            SimpleCsvFeatureLayer csvLayer = new SimpleCsvFeatureLayer(@"../../../Data/Csv/vehicle-route.csv");
+            SimpleCsvFeatureLayer csvLayer = new SimpleCsvFeatureLayer(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Data/Csv/vehicle-route.csv"));
 
             // Set the points image to an car icon and then apply it to all zoomlevels
-            PointStyle vehiclePointStyle = new PointStyle(new GeoImage(@"../../../Resources/vehicle-location.png"));
+            PointStyle vehiclePointStyle = new PointStyle(new GeoImage(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Resources/vehicle-location.png")));
             vehiclePointStyle.YOffsetInPixel = -12;
 
             csvLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = vehiclePointStyle;
