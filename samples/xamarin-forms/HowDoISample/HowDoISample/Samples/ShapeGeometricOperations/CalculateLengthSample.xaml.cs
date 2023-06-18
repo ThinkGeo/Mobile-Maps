f9e5d250e7bd820149e5cb9ea263d53f1d9ff1f2
Rui Li <ruili@thinkgeo.com>
2023-06-18 11:44:38 +0800

    thinkgeo/products/thinkgeosdk#560 Update CalculateLengthSample.xaml.cs

--- samples/xamarin-forms/HowDoISample/HowDoISample/Samples/ShapeGeometricOperations/CalculateLengthSample.xaml.cs
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/ShapeGeometricOperations/CalculateLengthSample.xaml.cs
@@ -35,37 +35,37 @@ namespace ThinkGeo.UI.XamarinForms.HowDoI
             backgroundOverlay.TileCache = new FileRasterTileCache(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ThinkGeoLightBackground");
             mapView.Overlays.Add(backgroundOverlay);
 
-            var friscoTrails = new ShapeFileFeatureLayer(Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                "Data/Shapefile/Hike_Bike.shp"));
-            var selectedLineLayer = new InMemoryFeatureLayer();
-            var layerOverlay = new LayerOverlay();
+            var friscoTrailsLayer = new ShapeFileFeatureLayer(Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Data/Shapefile/Hike_Bike.shp"));
 
             // Project friscoTrails layer to Spherical Mercator to match the map projection
-            friscoTrails.FeatureSource.ProjectionConverter = new ProjectionConverter(2276, 3857);
+            var projectionConverter = new ProjectionConverter(2276, 3857);
+            friscoTrailsLayer.FeatureSource.ProjectionConverter = projectionConverter;
 
-            // Style friscoTrails layer
-            friscoTrails.ZoomLevelSet.ZoomLevel01.DefaultLineStyle =
+            // Add a style to use to draw the friscoTrails layer
+            friscoTrailsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            friscoTrailsLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle =
                 LineStyle.CreateSimpleLineStyle(GeoColors.Orange, 2, false);
-            friscoTrails.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
 
-            // Style selectedLineLayer
+            var friscoTrailsOverlay = new LayerOverlay();
+            friscoTrailsOverlay.Layers.Add("FriscoTrailsLayer", friscoTrailsLayer);
+            mapView.Overlays.Add("FriscoTrailsOverlay", friscoTrailsOverlay);
+
+            // Create a layer to hold the selectedLineLayer found by the spatial query
+            var selectedLineLayer = new InMemoryFeatureLayer();
             selectedLineLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle =
                 LineStyle.CreateSimpleLineStyle(GeoColors.Green, 2, false);
             selectedLineLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
 
-            // Add friscoTrails layer to a LayerOverlay
-            layerOverlay.Layers.Add("friscoTrails", friscoTrails);
-
-            // Add selectedLineLayer to the layerOverlay
-            layerOverlay.Layers.Add("selectedLineLayer", selectedLineLayer);
+            var selectedLineOverlay = new LayerOverlay();
+            selectedLineOverlay.Layers.Add("SelectedLineLayer", selectedLineLayer);
+            mapView.Overlays.Add("SelectedLineOverlay", selectedLineOverlay);
 
             // Set the map extent
             mapView.CurrentExtent =
                 new RectangleShape(-10782307.6877106, 3918904.87378907, -10774377.3460701, 3912073.31442403);
 
             // Add LayerOverlay to Map
-            mapView.Overlays.Add("layerOverlay", layerOverlay);
 
             await mapView.RefreshAsync();
         }
@@ -75,19 +75,20 @@ namespace ThinkGeo.UI.XamarinForms.HowDoI
         /// </summary>
         private async void MapView_OnMapClick(object sender, TouchMapViewEventArgs e)
         {
-            var layerOverlay = (LayerOverlay) mapView.Overlays["layerOverlay"];
+            var friscoTrailsOverlay = (LayerOverlay) mapView.Overlays["FriscoTrailsOverlay"];
+            var friscoTrailsLayer = (ShapeFileFeatureLayer)friscoTrailsOverlay.Layers["FriscoTrailsLayer"];
 
-            var friscoTrails = (ShapeFileFeatureLayer) layerOverlay.Layers["friscoTrails"];
-            var selectedLineLayer = (InMemoryFeatureLayer) layerOverlay.Layers["selectedLineLayer"];
+            var selectedLineOverlay = (LayerOverlay)mapView.Overlays["SelectedLineOverlay"];
+            var selectedLineLayer = (InMemoryFeatureLayer)selectedLineOverlay.Layers["SelectedLineLayer"];
 
             // Query the friscoTrails layer to get the first feature closest to the map tap event
-            var feature = friscoTrails.QueryTools.GetFeaturesNearestTo(e.PointInWorldCoordinate, GeographyUnit.Meter, 1,
+            var feature = friscoTrailsLayer.QueryTools.GetFeaturesNearestTo(e.PointInWorldCoordinate, GeographyUnit.Meter, 1,
                 ReturningColumnsType.NoColumns).First();
 
             // Show the selected feature on the map
             selectedLineLayer.InternalFeatures.Clear();
             selectedLineLayer.InternalFeatures.Add(feature);
-            await layerOverlay.RefreshAsync();
+            await selectedLineOverlay.RefreshAsync();
 
             // Get the length of the first feature
             var length = ((LineBaseShape) feature.GetShape()).GetLength(GeographyUnit.Meter, DistanceUnit.Kilometer);
