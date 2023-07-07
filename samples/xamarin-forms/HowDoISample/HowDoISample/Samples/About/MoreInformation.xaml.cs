12c1486afbb19321b8ba4e5bd207a327c71fc4fa
Phil Thomas <philthomas@thinkgeo.com>
2023-07-07 10:33:54 -0500

    thinkgeo/products/thinkgeosdk#560 - Adding an 'About' page.

---
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/About/MoreInformation.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace ThinkGeo.UI.XamarinForms.HowDoI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MoreInformation : ContentPage
+    {
+        public MoreInformation()
+        {
+            InitializeComponent();
+        }
+
+        private async void Hyperlink_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync(((TappedEventArgs)e).Parameter.ToString());
+        }
+
+        private async void HyperlinkButton_Tapped(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync(((ImageButton)sender).BindingContext.ToString());
+        }
+    }
+}
