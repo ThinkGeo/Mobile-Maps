32b003773a5be0f956a3e78c5272380b57e6d5e6
Ben Bai <benbai@thinkgeo.com>
2023-07-09 18:03:21 -0500

    thinkgeo/products/thinkgeosdk#560
    Change "Sample Code" to "Source Code of this App"

--- samples/xamarin-forms/HowDoISample/HowDoISample/Samples/About/MoreInformation.xaml.cs
+++ samples/xamarin-forms/HowDoISample/HowDoISample/Samples/About/MoreInformation.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xamarin.Essentials;
 
 namespace ThinkGeo.UI.XamarinForms.HowDoI
 {
