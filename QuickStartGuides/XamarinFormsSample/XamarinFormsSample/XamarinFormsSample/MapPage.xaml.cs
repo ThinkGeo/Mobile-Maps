8d9a4ad1bd6ec83b2b2f0dc7c23114223173f585
Rui Li <ruili@thinkgeo.com>
2023-07-20 19:45:51 +0800

    thinkgeo/public/thinkgeo-mobile-maps#12 Build Mobile Maps(Android) Quick Start Guide Sample

---
+++ QuickStartGuides/XamarinFormsSample/XamarinFormsSample/XamarinFormsSample/MapPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using ThinkGeo.Core;
+using ThinkGeo.UI.XamarinForms;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace XamarinFormsSample
+{
+    public partial class MapPage : ContentPage
+    {    
+        public MapPage()
+        {
+            InitializeComponent();         
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            mapView.MapUnit = GeographyUnit.Meter;
+
+            var thinkGeoCloudVectorMapsOverlay = new ThinkGeoCloudVectorMapsOverlay("9ap16imkD_V7fsvDW9I8r8ULxgAB50BX_BnafMEBcKg~", "vtVao9zAcOj00UlGcK7U-efLANfeJKzlPuDB9nw7Bp4K4UxU_PdRDg~~", ThinkGeoCloudVectorMapsMapType.Light);
+            mapView.Overlays.Add(thinkGeoCloudVectorMapsOverlay);
+            thinkGeoCloudVectorMapsOverlay.TileCache =
+                new FileRasterTileCache(Path.Combine(FileSystem.CacheDirectory, "ThinkGeoCloudMapsOverlay"), "clientId");
+
+            mapView.RotationEnabled = true;
+            mapView.MapScale = mapView.ZoomLevelSet.ZoomLevel02.Scale;
+            mapView.CenterPoint = new PointShape(0, 0);
+            await mapView.RefreshAsync();
+        }
+    }
+}
