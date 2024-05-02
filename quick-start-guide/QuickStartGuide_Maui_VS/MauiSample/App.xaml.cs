8a9e66cbd50af52fca839fe669c61a9c56fdf884
Rui Li <ruili@thinkgeo.com>
2024-05-02 11:36:33 +0800

    thinkgeo/products/thinkgeosdk#685 Add quick start guide sample

---
+++ quick-start-guide/QuickStartGuide_Maui_VS/MauiSample/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ThinkGeo.UI.Maui;
+
+namespace MauiSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+
+        protected override async void OnStart()
+        {
+            await LicenseLoader.LoadLicense("mauidemo.thinkgeo.com.android.mapsuitelicense");
+            await LicenseLoader.LoadLicense("mauidemo.thinkgeo.com.ios.mapsuitelicense");            
+            MainPage = new AppShell();
+        }
+    }
+}
